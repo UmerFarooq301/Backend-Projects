01065584b6ad86dcff8e943a911255d7f2a99907
UmerFarooq301 <umerfarooq6981@gmail.com>
2025-10-17 19:10:55 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ dotenv.config({
 })
 
 connectDatabase()
+.then(() => {
+    app.listen((process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${
+            process.env.PORT }`);     
+    }))
+})
+.catch((error) =>{
+    console.log("MONGODB CONNECTION FAILED !!!", error);
+})
