9736f5914f090111df47ad78623bf1f48f1f23ba
UmerFarooq301 <umerfarooq6981@gmail.com>
2025-10-28 22:55:51 +0500

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDatabase from "./db/databaseConn.js";
+import {app} from './app.js'
 
 dotenv.config({
     path: './.env'
 })
 
+
 connectDatabase()
 .then(() => {
-    app.listen((process.env.PORT || 8000, () => {
-        console.log(`Server is running at port: ${
-            process.env.PORT }`);     
-    }))
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
 })
-.catch((error) =>{
-    console.log("MONGODB CONNECTION FAILED !!!", error);
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
